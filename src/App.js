9b5c24749791f04d361e9425f64a2f094786f2de
firasmindhi123 <firas.inception@gmail.com>
2024-06-27 13:26:38 +0530

    introduction

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./component/Expenseitem";
+function App() {
+  return (
+    <div>
+      <h1>let get started</h1>
+      <ExpenseItem></ExpenseItem>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Expenseitem.js
@@ -0,0 +1,16 @@
+function ExpenseItem()
+{
+  return (
+    <div>
+        <div>15 August 2023</div>
+        <div>Delhi</div>
+        <div>
+            <h1>Book</h1>
+            <div>$10</div>
+
+        </div>
+    </div>
+    
+  )
+}
+export default ExpenseItem
