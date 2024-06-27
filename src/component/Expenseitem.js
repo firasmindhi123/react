39de79fe3fa62eb2ae827f2e6a65d67297e86dd3
firasmindhi123 <firas.inception@gmail.com>
2024-06-27 20:18:00 +0530

    calendertypedate

--- src/component/Expenseitem.js
+++ src/component/Expenseitem.js
@@ -1,14 +1,24 @@
 import './expense.css'
-
+const months=['january','February','March','April','May','June','July','August','September','October','November','December']
 function ExpenseItem(props)
 {
+    const month=months[props.date.getMonth()]
+    const year=props.date.getFullYear()
+    const date=props.date.getDate()
+
+
   return (
-    <div>
-        <div className='expense-item'>{props.date.toISOString()}</div>
+    <div className='expense-item'>
+        
+        <div >
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{date}</div>
+        </div>
         <div className='expense-item__location'>{props.location}</div>
         <div className='expense-item__description'>
-            <h1>{props.title}</h1>
-            <div className='expense-item__price'>{props.price}</div>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.price}</div>
 
         </div>
     </div>
