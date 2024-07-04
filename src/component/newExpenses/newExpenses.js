cc19e4b164064a4e219076fa67747fc8c6136a53
firasmindhi123 <firas.inception@gmail.com>
2024-07-04 19:21:24 +0530

    render conditionally

--- src/component/newExpenses/newExpenses.js
+++ src/component/newExpenses/newExpenses.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 import ExpenseForm from "./ExpenseFrom"
 import "./newExpenses.css";
 
 const NewExpense = (props) => {
+  const [isEdit,setEdit]=useState(false)
     const saveExpenseHandler=(data)=>{
        const newdata={
         ...data,
         id:Math.random().toString()
        }
-      return props.onAppExpenseData(newdata)
+      props.onAppExpenseData(newdata)
+      setEdit(false)
+
 
     }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
+      {!isEdit && (<button onClick={()=>{
+         setEdit(true)
+      }}>AddExpense</button>)}
+      {isEdit && ( <ExpenseForm onSaveExpenseData={saveExpenseHandler} onCancel={()=>{
+        setEdit(false)
+      }} />)}
+     
     </div>
   );
 };
